f83c83440fefa1d0b08143eef6ac70aa82376cef
uffemcev <96709114+uffemcev@users.noreply.github.com>
2023-12-11 15:14:36 +0700

    Update and rename run.ps1 to script.ps1

--- script.ps1
+++ script.ps1
@@ -1,6 +1,8 @@
-$file = ".\Lethal Company.zip"
+$file = ".\LC.zip"
 $path = "C:\Program Files (x86)\Steam\steamapps\common"
 $mods = ".\Lethal Company"
+
+iwr 'https://github.com/uffemcev/LC/releases/download/LC/LC.zip' -Useb -OutFile '.\LC.zip'
 
 Remove-Item -Recurse -Force -ErrorAction 0 "$path\Lethal Company\BepInEx"
 Expand-Archive -ErrorAction 0 -Force $file $mods
